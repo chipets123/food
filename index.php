e6df73137e4d1e99c5a9175c7731db108eb5bb53
chisom <sommylight145@gmail.com>
2025-02-08 14:25:42 +0100

    food app

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php include 'layouts/header.html'; ?>
+
+<div class="container-fluid">
+    <div class="row border border-primary">
+        <div class="col-md-3 border border-dark p-2 mt-2">
+            <h2>Welcome!</h2>
+            <p>This s GESP food city😊.</p>
+        </div>
+            <div class="col-md-9 border border-success p-2 mt-2">
+                <h2>This is our menu</h2>
+                <p>Be sure to try our specials😊.</p>
+                <div class="col border border-danger p-2 mt-2">
+                    <div class="col border border-black p-2 mt-2">
+                    </div>
+                </div>
+            </div>
+    </div>
+</div>
+
+
+<?php include 'layouts/footer.html'; ?>
